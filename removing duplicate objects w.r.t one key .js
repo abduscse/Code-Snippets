96b6c84a6b602df2969e9eb13f8e655a9c5f011e
Abdus Sattar K M <AK00507516@techmahindra.com>
2019-01-05 11:42:34 +0530

    configurable key

--- removing duplicate objects w.r.t one key .js
+++ removing duplicate objects w.r.t one key .js
@@ -1,4 +1,8 @@
-var uniq = {}
-var arr  = [{"id":"1"},{"id":"1"},{"id":"2"}]
-var arrFiltered = arr.filter(obj => !uniq[obj.id] && (uniq[obj.id] = true));
-console.log('arrFiltered', arrFiltered)
+var arr  = [{"id":"1"},{"id":"1"},{"id":"2"}];
+
+const getUniqueObjects = (list, key) => {
+    var uniq = {}
+    return list.filter(obj => !uniq[obj[key]] && (uniq[obj[key]] = true));
+};
+
+console.log(getUniqueObjects(arr,'id'));
